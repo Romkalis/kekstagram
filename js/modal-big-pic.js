3ba9e45183c318658d8ddcfc4d94ed5e9dd3f29e
RLis <lisss17@mail.ru>
2023-07-12 16:31:03 +0300

    начал делать bigPic

---
+++ js/modal-big-pic.js
@@ -0,0 +1,9 @@
+// const picturesPreview = document.querySelector('.pictures');
+// const modalPicture = document.querySelector('.big-picture');
+
+// const showModal = function (evt) {
+// // modalPicture.classList.remove('hidden');
+
+// }
+
+// picturesPreview.addEventListener('click', showModal)
